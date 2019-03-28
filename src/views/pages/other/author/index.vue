0e9b829fc3a0a070efa4a5e59c43438e33a3d64e
smallH <472908522@qq.com>
2019-03-28 22:51:57 +0800

    更新

--- src/views/pages/other/author/index.vue
+++ src/views/pages/other/author/index.vue
@@ -3,8 +3,8 @@
 		<div class="bground">
 			<div class="title">作者资料</div>
 			<div class="line"></div>
-			<div class="content">黄泽平，在企业中历任项目经理、高级前端开发工程师、前端架构师，专注于前端领域研发与学习。​有7年前端开发工作经验，掌握 vue、react redux、react-native、nodejs、nwjs、webpack等多种前端技术及框架搭建经验，开发的项目范围有：web系统前端开发、rn移动应用开发、node后台服务开发、网站开发及桌面应用开发等，也曾有1年java后台服务开发经验。在GitHub上开源有多个框架和模块库。</div>
-			<div class="code-title">个人博客：</div>
+			<div class="content">黄泽平，在企业中历任项目经理、高级前端开发工程师、前端架构师，专注于前端领域研发与学习。​有7年前端开发工作经验，掌握 vue、react redux、react-native、nodejs、nwjs、webpack等多种前端技术及框架搭建经验，开发的项目范围有：web系统前端开发、rn移动应用开发、node后台服务开发、网站开发及桌面应用开发等，也曾有1年java后台服务开发经验。在GitHub上开源有前端框架和模块库。</div>
+			<div class="code-title">个人技术博客：</div>
 			<div>http://blog.csdn.net/zeping891103</div>
 			<div class="code-title">微信号：</div>
 			<img :src="imgUrl" class="img" />
